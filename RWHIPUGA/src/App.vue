165318bd18304d67b81d019cea2cc9430645841c
Pres-010 <presleymukunzinshuti@gmail.com>
2026-04-29 14:58:10 +0200

    Add the whole game

---
+++ RWHIPUGA/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Truth in the Pictures</h1>
+    <PuzzleBoard />
+  </div>
+</template>
+
+<script>
+import PuzzleBoard from './components/PuzzleBoard.vue'
+
+export default {
+  components: { PuzzleBoard }
+}
+</script>
+
